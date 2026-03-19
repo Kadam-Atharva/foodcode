f1a93510f1559fd8a06325f1dc142cd99037cde6
Atharva Kadam <atharvakadam751@gmail.com>
2026-03-19 20:23:04 +0530

    New Backend

---
+++ backend/src/main/java/com/foodcode/foodcode/FoodcodeApplication.java
@@ -0,0 +1,13 @@
+package com.foodcode.foodcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodcodeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/foodcode/foodcode/FoodcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodcode.foodcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
